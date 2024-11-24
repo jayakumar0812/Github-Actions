5de512de28e9ee5faf9acf51db2d03d097a3f575
jayakumar0812 <L00187728@atu.ie>
2024-11-24 12:36:21 +0000

    Added files calculator functions with test cases 24

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sample-python-app",
+    version="1.0.0",
+    description="A sample Python application to demonstrate GitHub Actions",
+    author="Jay",
+    packages=find_packages(),
+    install_requires=[],
+)
